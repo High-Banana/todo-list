13fd47c7490b7ecce03d67f594f97a7980a80017
Potassium <brainiacgamer444@gmail.com>
2023-09-05 23:37:41 +0545

    Make seperate module/file to create form from javascript instead of html and display/hide form

--- src/modules/Form.js
+++ src/modules/Form.js
@@ -1,9 +1,43 @@
-export default function loadInputField() {
+export default function loadForm() {
+    const projectContainer = document.querySelector(".project-container");
+    projectContainer.appendChild(createForm());
+}
+
+export function showForm() {
     const projectForm = document.querySelector(".project-form");
     projectForm.style.display = "flex";
 }
 
-export function removeInputField() {
+export function removeForm() {
     const projectForm = document.querySelector(".project-form");
     projectForm.style.display = "none";
+}
+
+function createForm() {
+
+    const projectForm = document.createElement("div");
+    projectForm.classList.add("project-form");
+
+    const input = document.createElement("input");
+    input.setAttribute("id", "project-title-input");
+    input.placeholder = "Project Title";
+
+    const projectFormButton = document.createElement("div");
+    projectFormButton.classList.add("project-form-button");
+
+    const addButton = document.createElement("button");
+    addButton.textContent = "Add";
+    addButton.classList.add("addButton");
+
+    const cancelButton = document.createElement("button");
+    cancelButton.textContent = "Cancel";
+    cancelButton.classList.add("cancelButton");
+
+    projectFormButton.appendChild(addButton);
+    projectFormButton.appendChild(cancelButton);
+
+    projectForm.appendChild(input);
+    projectForm.appendChild(projectFormButton);
+
+    return projectForm;
 }
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,18 +1,19 @@
-import loadInputField, {removeInputField} from "./Form";
+import loadForm, {removeForm, showForm} from "./Form";
 
 export default function updateDom() {
+    window.addEventListener("load", loadForm);
     const addProjectButton = document.querySelector(".add-project-button");
     
     addProjectButton.addEventListener("click", () => {
         addProjectButton.style.display = "none";
-        loadInputField();
+        showForm();
+        const cancelButton = document.querySelector(".cancelButton");
+    
+        cancelButton.addEventListener("click", () => {
+            addProjectButton.style.display = "flex";
+            removeForm();
+        })
     });
 
-    const cancelButton = document.querySelector(".cancelButton");
-    
-    cancelButton.addEventListener("click", () => {
-        addProjectButton.style.display = "flex";
-        removeInputField();
-    })
 
 }
