7e732d16e3347940e754c32c0c95dae2aaf1e88f
Potassium <brainiacgamer444@gmail.com>
2023-09-06 13:26:16 +0545

    Make various functions for various activities for better readablity

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,32 +4,54 @@ import Project from "./project";
 export default function updateDom() {
     window.addEventListener("load", loadForm);
     const addProjectButton = document.querySelector(".add-project-button");
+    let formDisplayed = false;
 
-    addProjectButton.addEventListener("click", () => {
+    function displayProjectForm() {
         addProjectButton.style.display = "none";
         showForm();
+        formDisplayed = true;
+    }
 
-        function createProject() {
-            const inputField = document.getElementById("project-title-input");
-            const project = new Project(inputField.value);
-            project.setProjectList(project.title);
-        }
+    function getProject() {
+        const inputField = document.getElementById("project-title-input");
+        const project = new Project(inputField.value);
+        project.setProjectList(project.title);
+        return project;
+    }
 
-        function removeForm() {
-            addProjectButton.style.display = "flex";
-            const projectForm = document.querySelector(".project-form");
-            projectForm.style.display = "none";
-        }
+    function createProjectList() {
+        const projectContainer = document.querySelector(".project-container");
+        const projectList = document.createElement("button");
+        projectList.textContent = getProject().title;
+        projectContainer.appendChild(projectList);
+    }
 
-        // After form has been displayed
-        const addBbutton = document.querySelector(".addButton");
-        addBbutton.addEventListener("click", createProject);
+    function removeForm() {
+        addProjectButton.style.display = "flex";
+        const projectForm = document.querySelector(".project-form");
+        projectForm.style.display = "none";
+        formDisplayed = false;
+        const inputField = document.getElementById("project-title-input");
+        inputField.textContent = "";
+    }
+
+    function addButtonHandler() {
+        const addButton = document.querySelector(".addButton");
+        addButton.addEventListener("click", () => {
+            if (formDisplayed) {
+                createProjectList();
+                removeForm();
+            }
+        });
         document.addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                createProject()
+            if (event.key === "Enter" && formDisplayed) {
+                createProjectList();
+                removeForm();
             };
         });
+    }
 
+    function cancelButtonHandler() {
         const cancelButton = document.querySelector(".cancelButton");
         cancelButton.addEventListener("click", removeForm);
         document.addEventListener("keydown", (event) => {
@@ -37,5 +59,11 @@ export default function updateDom() {
                 removeForm()
             };
         });
+    }
+
+    addProjectButton.addEventListener("click", () => {
+        displayProjectForm();
+        addButtonHandler();
+        cancelButtonHandler();
     });
 }
